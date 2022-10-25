074c4336cda7124434373cc3030323dba763f0df
Upal Barua <upalbarua52@gmail.com>
2022-10-25 09:58:52 +0600

    Added 404 page!

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layouts/Main.jsx';
+import ErrorPage from '../components/ErrorPage/ErrorPage';
 
-export const router = createBrowserRouter([{ path: '/', element: <Main /> }]);
+export const router = createBrowserRouter([
+  { path: '/', element: <Main /> },
+  { path: '*', element: <ErrorPage /> },
+]);
